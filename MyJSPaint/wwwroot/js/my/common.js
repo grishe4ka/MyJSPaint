f010a74d1d229f6c09a8640e3eee3320cd323849
Ryhor Samoshkin <ryhor.samoshkin@innowise-group.com>
2021-04-01 17:16:19 +0300

    Add project files.

---
+++ MyJSPaint/wwwroot/js/my/common.js
@@ -0,0 +1,35 @@
+﻿
+const scale = 10;
+
+function historyUpdateState(state, action) {
+    if (action.undo == true) {
+        if (state.done.length == 0) return state;
+        return Object.assign({}, state, {
+            picture: state.done[0],
+            done: state.done.slice(1),
+            doneAt: 0
+        });
+    } else if (action.picture &&
+        state.doneAt < Date.now() - 1000) {
+        return Object.assign({}, state, action, {
+            done: [state.picture, ...state.done],
+            doneAt: Date.now()
+        });
+    } else {
+        return Object.assign({}, state, action);
+    }
+}
+
+//function updateState(state, action) {
+//    return Object.assign({}, state, action);
+//}
+
+function elt(type, props, ...children) {
+    let dom = document.createElement(type);
+    if (props) Object.assign(dom, props);
+    for (let child of children) {
+        if (typeof child != "string") dom.appendChild(child);
+        else dom.appendChild(document.createTextNode(child));
+    }
+    return dom;
+}
---
+++ MyJSPaint/wwwroot/js/my/tools.js
@@ -0,0 +1,98 @@
+﻿function draw(pos, state, dispatch) {
+    function drawPixel({ x, y }, state) {
+        let drawn = { x, y, color: state.color };
+        dispatch({ picture: state.picture.draw([drawn]) });
+    }
+    drawPixel(pos, state);
+    return drawPixel;
+}
+
+function rectangle(start, state, dispatch) {
+    function drawRectangle(pos) {
+        let xStart = Math.min(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
+        let xEnd = Math.max(start.x, pos.x);
+        let yEnd = Math.max(start.y, pos.y);
+        let drawn = [];
+        for (let y = yStart; y <= yEnd; y++) {
+            for (let x = xStart; x <= xEnd; x++) {
+                drawn.push({ x, y, color: state.color });
+            }
+        }
+        dispatch({ picture: state.picture.draw(drawn) });
+    }
+    drawRectangle(start);
+    return drawRectangle;
+}
+
+function circle(start, state, dispatch) {
+    function drawCircle(pos) {
+        let xStart = Math.min(start.x, pos.x);
+        let xEnd = Math.max(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
+        let drawn = [];
+        var xRadius = xEnd - xStart;
+        //console.log("xRadius=", xRadius, " yRadius=", yRadius);
+        for (let y = -xRadius; y <= xRadius; y++) {
+            for (let x = -xRadius; x <= xRadius; x++) {
+                if (x * x + y * y <= xRadius * xRadius) {
+                    //console.log("ADD POINT");
+                    drawn.push({ x: xStart + x, y : yStart + y, color: state.color });
+                    //setpixel(origin.x + x, origin.y + y);
+                }
+            }
+        }
+        dispatch({ picture: state.picture.draw(drawn) });
+    }
+    drawCircle(start);
+    return drawCircle;
+}
+
+function ellipse(start, state, dispatch) {
+    function drawEllipse(pos) {
+        let xStart = Math.min(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
+        let xEnd = Math.max(start.x, pos.x);
+        let yEnd = Math.max(start.y, pos.y);
+        let drawn = [];
+        var xRadius = xEnd - xStart;
+        var yRadius = yEnd - yStart;
+        console.log("xRadius=", xRadius, " yRadius=", yRadius);
+        for (let y = -yRadius; y <= yRadius; y++) {
+            for (let x = -xRadius; x <= xRadius; x++) {
+                if (x * x + y * y <= xRadius * yRadius) {
+                    console.log("ADD POINT");
+                    drawn.push({ x: xStart + x, y: yStart + y, color: state.color });
+                    //setpixel(origin.x + x, origin.y + y);
+                }
+            }
+        }
+        dispatch({ picture: state.picture.draw(drawn) });
+    }
+    drawEllipse(start);
+    return drawEllipse;
+}
+
+const around = [{ dx: -1, dy: 0 }, { dx: 1, dy: 0 },
+{ dx: 0, dy: -1 }, { dx: 0, dy: 1 }];
+
+function fill({ x, y }, state, dispatch) {
+    let targetColor = state.picture.pixel(x, y);
+    let drawn = [{ x, y, color: state.color }];
+    for (let done = 0; done < drawn.length; done++) {
+        for (let { dx, dy } of around) {
+            let x = drawn[done].x + dx, y = drawn[done].y + dy;
+            if (x >= 0 && x < state.picture.width &&
+                y >= 0 && y < state.picture.height &&
+                state.picture.pixel(x, y) == targetColor &&
+                !drawn.some(p => p.x == x && p.y == y)) {
+                drawn.push({ x, y, color: state.color });
+            }
+        }
+    }
+    dispatch({ picture: state.picture.draw(drawn) });
+}
+
+function pick(pos, state, dispatch) {
+    dispatch({ color: state.picture.pixel(pos.x, pos.y) });
+}
